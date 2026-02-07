ed2d527f0adcaf443fb77e8cc5033b0650030790
Vaibhav Bhargava <dpen75437@gmail.com>
2026-02-07 23:36:08 +0530

    Add files via upload

---
+++ numpee4.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+x_values = np.arange(0,1005,5)
+print(x_values)#provides stepsize beginning and the step size
+
+x_values = np.linspace(0,1000,1001)
+print(x_values)#provides how many values we want to have
