32326d0daadfd2c1528578f2b95a823c542c40e8
Vaibhav Bhargava <dpen75437@gmail.com>
2026-02-19 10:37:32 +0530

    Add files via upload

---
+++ numpee13.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = np.array([[1,2,3,4,5,6],
+              [7,8,9,10,11,12],
+              [13,14,15,16,17,18],
+              [18,19,20,21,22,23]])
+
+#print(a.min())
+#print(a.max())
+#print(a.mean())
+#print(a.std())
+#print(a.sum())
+#print(np.median(a))
